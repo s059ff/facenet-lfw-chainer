1f6ce10ae2c03c0a03785bc4fc391b745e03ed99
Tomohiro Namba <s059ff@yamaguchi-u.ac.jp>
2019-02-09 17:26:00 +0900

    .

---
+++ README.md
@@ -0,0 +1,35 @@
+# facenet-mnsit-chainer
+MNIST データセットを使用した FaceNet による手書き数字画像の類似度推定 及び クラスタリングのテストです。
+
+## 実行環境
+* Ubuntu 18.04 64bit LTS
+* Python 3.7.2
+* Chainer 5.2.0
+* cupy 5.2.0
+
+## 実行方法
+* モデルのトレーニング
+
+    ```bash
+    python train.py
+    ```
+
+* クラスタリング結果 可視化 (PCA により 128 次元から 2 次元へ圧縮して表示します)
+
+    ```bash
+    python visualize_embeddings.py -s (モデルのトレーニングで作成された embeddings-*.npy のパス)
+    ```
+
+* クラスタリング結果 サンプル間類似度(距離) 可視化
+
+    ```bash
+    python visualize_distances.py -s (モデルのトレーニングで作成された embeddings-*.npy のパス)
+    ```
+
+## 実行結果
+* クラスタリング結果
+![](https://github.com/s059ff/facenet-mnist-chainer/blob/master/examples/embeddings-100.png)
+
+* サンプル間距離 推定 結果
+![](https://github.com/s059ff/facenet-mnist-chainer/blob/master/examples/embeddings-100-choices.png)
+![](https://github.com/s059ff/facenet-mnist-chainer/blob/master/examples/embeddings-100-distances.png)
---
+++ visualize_distances.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+
+import chainer
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.decomposition import PCA
+
+import functions
+
+
+def main():
+
+    # Parse arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source', **{
+        'type': str,
+        'help': 'The embeddings.npy file path.',
+        'required': True
+    })
+    args = parser.parse_args()
+
+    # Load embeddings file.
+    embeddings = np.load(args.source)
+
+    # Compress each embeddings to 2 dimension using PCA.
+    if 2 < embeddings.shape[1]:
+        pca = PCA(n_components=2)
+        pca.fit(embeddings)
+
+    # Load inputs data.
+    _, val = chainer.datasets.get_mnist(ndim=3)
+    batches = np.array([x for x, y in val])
+    labels = np.array([y for x, y in val])
+
+    indices = np.ravel([np.random.choice(np.ravel(np.argwhere(labels == i)), size=3) for i in range(10)])
+    titles = [f'{i}-{j + 1}' for i in range(10) for j in range(3)]
+
+    for i, title, index in zip(range(len(indices)), titles, indices):
+        batch = batches[index]
+        plt.subplot(5, 6, (i + 1))
+        plt.subplots_adjust(hspace=0.5)
+        plt.imshow(np.reshape(batch, (28, 28)))
+        plt.gray()
+        plt.title(title, pad=2.0)
+        plt.xticks(color=str(None))
+        plt.yticks(color=str(None))
+        plt.tick_params(length=0)
+    head, ext = os.path.splitext(args.source)
+    plt.savefig(f'{head}-choices.png')
+
+    pairwise_distances = functions._pairwise_distances(embeddings[indices]).data
+    np.savetxt(f'{head}-distances.txt', pairwise_distances, fmt='%.2f')
+    plt.close()
+
+    plt.imshow(pairwise_distances)
+    plt.xticks(np.arange(0, 30 + 1, 1), labels=titles, rotation=-90)
+    plt.yticks(np.arange(0, 30 + 1, 1), labels=titles)
+    plt.savefig(f'{head}-distances.png')
+
+if __name__ == '__main__':
+    main()
