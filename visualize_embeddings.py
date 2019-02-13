671a2aacfc525c8d2d7c85ea783fef686c0603eb
Namba_Tomohiro <Namba_Tomohiro@ad.konami>
2019-02-13 18:38:11 +0900

    Enable to compress by t-SNE.

--- visualize_embeddings.py
+++ visualize_embeddings.py
@@ -5,6 +5,7 @@ import chainer
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 
 def main():
@@ -16,6 +17,11 @@ def main():
         'help': 'The embeddings.npy file path.',
         'required': True
     })
+    parser.add_argument('-a', '--algorithm', **{
+        'type': str,
+        'help': 'Compression algorithm. "pca" or "tsne". default: pca.',
+        'default': 'pca'
+    })
     args = parser.parse_args()
 
     # Load embeddings file.
@@ -23,22 +29,27 @@ def main():
 
     # Compress each embeddings to 2 dimension using PCA.
     if 2 < embeddings.shape[1]:
-        pca = PCA(n_components=2)
-        pca.fit(embeddings)
+        compressor = {
+            'pca': PCA,
+            'tsne': TSNE
+        }[args.algorithm]
+        transforms = compressor(n_components=2).fit_transform(embeddings)
+    else:
+        transforms = embeddings
 
     # Load label data.
     _, val = chainer.datasets.get_mnist(ndim=3)
     labels = np.array([y for x, y in val])
 
-    assert (len(labels) == len(embeddings))
+    assert (len(labels) == len(transforms))
 
     for label in range(10):
         indices = np.where(labels == label)
-        x, y = np.ravel(embeddings[indices, 0]), np.ravel(embeddings[indices, 1])
+        x, y = np.ravel(transforms[indices, 0]), np.ravel(transforms[indices, 1])
         plt.scatter(x, y, label=str(label))
     plt.legend()
     head, ext = os.path.splitext(args.source)
-    plt.savefig(f'{head}.png')
+    plt.savefig(f'{head}-{args.algorithm}.png')
 
 
 if __name__ == '__main__':
